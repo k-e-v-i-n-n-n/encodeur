79ba483f8b57b3b2d84a759d9757ca32e613ad43
kevin breid <kevinbreid@yahoo.com>
2022-05-30 15:42:53 -0700

    Personal Site Updates

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const box = document.getElementById('box');
+
+box.addEventListener('mouseover', function handleMouseOver() {
+  box.style.color = 'red';
+});
+
+box.addEventListener('mouseout', function handleMouseOut() {
+  box.style.color = 'black';
+});
